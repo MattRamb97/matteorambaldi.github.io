8626a106c7308afc3f04d0020b790e53c6c76a97
MattRamb97 <106387960+MattRamb97@users.noreply.github.com>
2024-10-18 13:30:45 +0200

    Update mr.js

--- assets/js/mr.js
+++ assets/js/mr.js
@@ -18,7 +18,50 @@ $(document).ready(function(){
     });
 });
 
-// protfolio filters
+// Chart skills
+document.addEventListener('DOMContentLoaded', function() {
+    var ctx = document.getElementById('skillsChart').getContext('2d');
+    var skillsChart = new Chart(ctx, {
+        type: 'bar', // Choose the type of chart (bar, pie, radar, etc.)
+        data: {
+            labels: ['C', 'C++', 'C#', 'Java', 'Python', 'Matlab', 'Ros'], // Your skills
+            datasets: [{
+                label: 'Skill Level (%)',
+                data: [70, 80, 95, 70, 85, 95, 65], // Corresponding skill levels
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(100, 255, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(100, 255, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    max: 100
+                }
+            }
+        }
+    });
+});
+
+
+// portfolio filters
 $(window).on("load", function() {
     var t = $(".portfolio-container");
     t.isotope({
